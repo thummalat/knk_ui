888b83bd5f42e3ba5c38b1576c6dbc46b3b5d29c
Thanuz Thummala <tummala.royels@gmail.com>
2025-02-08 23:45:40 -0500

    adding basic from for adding order details

---
+++ src/components/orders/AddOrderDetails.tsx
@@ -0,0 +1,64 @@
+import { TextField } from "@mui/material";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Grid from "@mui/material/Grid2";
+
+interface IAddOrderDetails {
+  open: boolean;
+  closeCallBack: () => void;
+}
+
+export default function AddOrderDetails({
+  open,
+  closeCallBack,
+}: IAddOrderDetails) {
+  const handleClose = () => {
+    closeCallBack();
+  };
+
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>Order Details</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Enter new order details below
+          <Grid container spacing={2} sx={{ paddingTop: "1rem" }}>
+            <Grid size={12}>
+              <TextField
+                fullWidth
+                label="Customer Name"
+                name="cusotmerName"
+              ></TextField>
+            </Grid>
+            <Grid size={6}>
+              <TextField
+                fullWidth
+                type="number"
+                label="# Sold"
+                name="quantitiesSold"
+              ></TextField>
+            </Grid>
+            <Grid size={6}>
+              <TextField
+                fullWidth
+                type="number"
+                label="Sold Price"
+                name="soldPrice"
+              ></TextField>
+            </Grid>
+          </Grid>
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button type="submit" variant="contained">
+          Save
+        </Button>
+        <Button onClick={handleClose}>Cancel</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
