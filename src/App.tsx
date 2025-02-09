f900062f4a41aac9685b9efea72e70aad3e2a287
Thanuz Thummala <tummala.royels@gmail.com>
2025-02-09 14:14:00 -0500

    Adding inventory details

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { CssBaseline } from "@mui/material";
 import Header from "./components/Header";
 import Orders from "./components/Orders";
+import InventoryDetails from "./components/InventoryDetails";
 
 const App = () => {
   return (
@@ -8,9 +9,7 @@ const App = () => {
       <CssBaseline />
       <Header />
       <Orders />
-      {/* <Herosection />
-      <Discountsection />
-      <Newarrivalsection /> */}
+      <InventoryDetails />
     </>
   );
 };
