02f8734d98e322fd5b7c05f69da855011e4545b5
thanuz thummala <thanuz.thummala@oracle.com>
2026-02-25 11:38:21 -0500

    removing unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Container, CssBaseline } from "@mui/material";
 import Header from "./components/Header";
 
-import InventoryDetails from "./components/inventory/InventoryDetails";
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Orders from "./components/orders/Orders";
 import Fin from "./components/fin/Fin";
